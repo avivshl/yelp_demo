083ed20fc6bd0655b53b4752eec53d5d63b53b5f
Raghu Betina <raghu@firstdraft.com>
2016-11-10 02:12:42 +0000

    Direct association between reviews and reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,13 @@
 class Review < ApplicationRecord
   # Direct associations
 
+  belongs_to :review_parent,
+             :class_name => "Review"
+
+  has_many   :sub_reviews,
+             :class_name => "Review",
+             :foreign_key => "review_parent_id"
+
   has_many   :review_photos,
              :dependent => :destroy
 
