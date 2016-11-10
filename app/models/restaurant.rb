abef7580b629b27538cbcfc96c985114323220b9
Raghu Betina <raghu@firstdraft.com>
2016-11-10 02:12:42 +0000

    Indirect association between restaurants and cities

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -13,6 +13,10 @@ class Restaurant < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :city,
+             :through => :restaurants,
+             :source => :city
+
   # Validations
 
 end
